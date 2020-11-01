24d3eeed60c959e1e358c12855c2a62f73672e86
DaMIRka-Oo <54149463+DaMIRka-Oo@users.noreply.github.com>
2020-11-01 18:04:21 +0300

    Update SignIn.js

--- src/SignIn.js
+++ src/SignIn.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React,{Component} from 'react';
 import logo from './image/logo.svg';
 import './App.css';
 
 function onClickEnter(){
     window.location.assign('http://localhost:3000/start/');
 }
+
 function onClickRegistrate(){
   window.location.assign('http://localhost:3000/registration/');
 }
@@ -12,38 +13,99 @@ function onClickForgot(){
     window.location.assign('http://localhost:3000/failed-to-login/');
 }
 
-function SignIn() {
-  return (
-    <div className="wrapper-sign-in">
-      <section className="section-form">
-          <main>
-            <form className="sign-in-form" action="" method="post">
-              <a href="https://aventi.ca">
-                <img src={logo} alt="Логотип Aventica Office Scanner"/>
-              </a>
-              <input className="user-info" id="user-surname-id" type="text" name="surname" defaultValue=""
-                     placeholder="Адрес электронной почты" />
-              <input className="user-info" id="user-name-id" type="password" name="name" defaultValue=""
-                     placeholder="Пароль"/>
-                  <input className="user-login" id="sign-in-id" type="button" name="sign-in" value="Войти"  onClick={(e) => onClickEnter(e)}/>
-                    <a className="forgot_password " onClick={(e) => onClickForgot(e)}>Забыли пароль?</a>
-                    <hr/>
-                      <a className="registr-but" onClick={(e) => onClickRegistrate(e)}>Зарегистрироваться</a>
-            </form>
-          </main>
-      </section>
-        <footer className="footer">
-            <div className="footer-container">
-                <p className="footer-info">E-mail: hello@aventica.ru</p>
-                <p className="footer-info">Телефон: 8(499)653-50-67</p>
-                <a href="https://www.facebook.com/Aventicalab/" className="footer-link">
-                    <i className="fab fa-facebook-square"/>
-                </a>
-                <p className="footer-info">© CodeBusters, 2020</p>
+class SignIn extends React.Component{
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            SendMail:{
+                email: '',
+                password: ''
+            },
+
+            topicOptions: ['Отзыв','Вопрос','Претензия','Иное'],
+            skillOptions: ['Согласен']
+
+        }
+        this.handleEmail = this.handleEmail.bind(this);
+        this.handlePassword = this.handlePassword.bind(this);
+        this.handleSendMail = this.handleSendMail.bind(this);
+
+    }
+
+    handleEmail(e) {
+        let value = e.target.value;
+        this.setState( prevState => ({ SendMail :
+                {...prevState.SendMail, email: value
+                }
+        }), () => console.log(this.state.SendMail))
+    }
+
+    handlePassword(e) {
+        let value = e.target.value;
+        this.setState( prevState => ({ SendMail :
+                {...prevState.SendMail, password: value
+                }
+        }), () => console.log(this.state.SendMail))
+    }
+
+
+    handleSendMail(e) {
+        e.preventDefault();
+        let userData = this.state.SendMail;
+
+        fetch('https://www.google.ru/',{
+            method: "POST",
+            body: JSON.stringify(userData),
+            headers: {
+                'Accept': 'application/json'
+            },
+        }).then(response => {
+            response.json().then(data =>{
+                console.log("Successful" + data);
+            })
+        })
+
+        onClickEnter()
+    }
+
+
+
+    render() {
+        return (
+            <div className="wrapper-sign-in">
+                <section className="section-form">
+                    <main>
+                        <form className="sign-in-form" action="" method="post">
+                            <a href="https://aventi.ca">
+                                <img src={logo} alt="Логотип Aventica Office Scanner"/>
+                            </a>
+                            <input className="user-info" id="user-surname-id" type="text" name="surname" value={this.state.SendMail.email} onChange={this.handleEmail}
+                                   placeholder="Адрес электронной почты"/>
+                            <input className="user-info" id="user-name-id" type="password" name="name" value={this.state.SendMail.password} onChange={this.handlePassword}
+                                   placeholder="Пароль"/>
+                            <input className="user-login" id="sign-in-id" type="submit" name="sign-in" value="Войти"
+                                   onSubmit={this.handleSendMail}/>
+                            <a className="forgot_password " onClick={(e) => onClickForgot(e)}>Забыли пароль?</a>
+                            <hr/>
+                            <a className="registr-but" onClick={(e) => onClickRegistrate(e)}>Зарегистрироваться</a>
+                        </form>
+                    </main>
+                </section>
+                <footer className="footer">
+                    <div className="footer-container">
+                        <p className="footer-info">E-mail: hello@aventica.ru</p>
+                        <p className="footer-info">Телефон: 8(499)653-50-67</p>
+                        <a href="https://www.facebook.com/Aventicalab/" className="footer-link">
+                            <i className="fab fa-facebook-square"/>
+                        </a>
+                        <p className="footer-info">© CodeBusters, 2020</p>
+                    </div>
+                </footer>
             </div>
-        </footer>
-    </div>
-  );
+        );
+    }
 }
 
 export default SignIn;
